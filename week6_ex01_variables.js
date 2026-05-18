43fb337f1a337b273107a92f51699223cb6f862c
Thabo Joseph Banda <tb3508771@gmail.com>
2026-05-19 00:05:13 +0200

    Add Exercise 01 - Variables and String Manipulation

---
+++ week6_ex01_variables.js
@@ -0,0 +1,12 @@
+const firstName = "Joseph";
+const lastName = "Banda";
+
+let age = 20;
+
+const fullName = firstName + " " + lastName;
+
+console.log("Hello, my name is " + fullName);
+
+age = 21;
+
+console.log("I am now " + age + " years old");
