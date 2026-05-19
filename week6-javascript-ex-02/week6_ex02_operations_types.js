d07ff7c9daad6d4cc3e56424f6dfda5d324b9fc3
Thabo Joseph Banda <tb3508771@gmail.com>
2026-05-19 19:42:01 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ week6-javascript-ex-02/week6_ex02_operations_types.js
@@ -0,0 +1,10 @@
+let number1 = 6;
+let number2 = 10;
+
+let number1String = String(number1);
+
+let sum = number1 + number2;
+let product = number1 * number2;
+
+console.log(sum);
+console.log(product);
