8ae73e294b5fd6f5f3abf0a447ad98a05c5688d8
Thabo Joseph Banda <tb3508771@gmail.com>
2026-05-20 15:33:16 +0200

    Add Exercise 03 - Array Basics

---
+++ week6-javascript-ex-03/week6_ex03_arrays.js
@@ -0,0 +1,14 @@
+let colors = [];
+
+colors.push("green");
+colors.push("red");
+colors.push("yellow";)
+
+let numbers = [1, 2, 3, 4, 5];
+
+colors.pop("yellow");
+
+colors.unshift("black");
+
+console.log(colors.length);
+console.log(numbers.length);
