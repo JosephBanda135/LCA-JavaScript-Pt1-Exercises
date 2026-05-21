8308fe8eda51a9d7e5fe7a37fdd9a23557777eb3
Thabo Joseph Banda <tb3508771@gmail.com>
2026-05-21 16:25:39 +0200

    Add Exercise 04 - Conditional Logic

---
+++ week6-javascript-ex-04/week6_ex04_conditionals.js
@@ -0,0 +1,13 @@
+let score = 75;
+
+let passingGrade = 60;
+
+if (score >= passingGrade) {
+  console.log("Passed");
+} else {
+  console.log("Failed");
+}
+
+if (score === 100) {
+  console.log("Perfect");
+}
